609894e5628bd44ff720e0df6fa0f83032b2f55d
Zohreh <zohrehasadi2018@yahoo.com>
2026-01-13 12:04:29 +0100

    Add essential dependencies for the project and task 2 files
    Signed-off-by: Zohreh <zohrehasadi2018@yahoo.com>

---
+++ homework2-numpy/homework2-numpy-tozip/minified.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import ast
+import inspect
+import sys
+from functools import wraps
+
+rujGomsJxc = tuple
+rujGomsJxz = sum
+rujGomsJxf = isinstance
+rujGomsJxM = print
+rujGomsJxt = SyntaxError
+rujGomsJxB = AttributeError
+rujGomsJhW = True
+rujGomsJhx = enumerate
+rujGomsJhN = int
+rujGomsJhH = str
+rujGomsJhF = len
+rujGomsJhi = ord
+rujGomsJhe = set
+rujGomsJhT = False
+rujGomsJhE = list
+rujGomsJhn = None
+rujGomsJhQ = range
+rujGomsJhb = open
+rujGomsJhK = zip
+rujGomsJhA = dict
+rujGomsJhO = next
+rujGomsJhC = StopIteration
+
+
+def no_imports(func):
+    try:
+        rujGomsJWx = ast.walk(ast.parse(inspect.getsource(func)))
+        rujGomsJWh = (ast.Import, ast.ImportFrom)
+        rujGomsJWN = rujGomsJxc(rujGomsJWx)
+        rujGomsJWH = rujGomsJxz(rujGomsJxf(node, rujGomsJWh) for node in rujGomsJWN)
+
+        if rujGomsJWH > 0:
+            rujGomsJxM(
+                "Your function contains {} import".format(rujGomsJWH)
+                + f" statement{'s' if rujGomsJWH != 1 else ''} which is not allowed"
+            )
+    except rujGomsJxt:
+        rujGomsJWH = -1
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    def assert_no_imports():
+        assert rujGomsJWH == 0, "No import statements allowed"
+
+    wrapper.assert_no_imports = assert_no_imports
+    return wrapper
+
+
+def max_allowed_loops(rujGomsJWT):
+    def decorator(func):
+        try:
+            rujGomsJWi = (
+                ast.For,
+                ast.While,
+                ast.AsyncFor,
+                ast.ListComp,
+                ast.DictComp,
+                ast.SetComp,
+                ast.GeneratorExp,
+            )
+            rujGomsJWx = ast.walk(ast.parse(inspect.getsource(func)))
+            rujGomsJWN = rujGomsJxc(rujGomsJWx)
+            rujGomsJWe = rujGomsJxz(rujGomsJxf(node, rujGomsJWi) for node in rujGomsJWN)
+            if rujGomsJWe > rujGomsJWT:
+                rujGomsJxM(
+                    "Your function uses more loops than allowed"
+                    "\nMax allowed: {}\nYour function: {}".format(
+                        rujGomsJWT, rujGomsJWe
+                    ),
+                    file=sys.stderr,
+                )
+        except rujGomsJxt:
+            rujGomsJWe = -1
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        def assert_not_too_many_loops():
+            assert (
+                rujGomsJWT >= rujGomsJWe
+            ), "Function uses too many loops: allowed: {},".format(
+                rujGomsJWT
+            ) + " used: {}".format(
+                rujGomsJWe
+            )
+
+        wrapper.assert_not_too_many_loops = assert_not_too_many_loops
+        return wrapper
+
+    return decorator
+
+
+no_loops_allowed = max_allowed_loops(0)
